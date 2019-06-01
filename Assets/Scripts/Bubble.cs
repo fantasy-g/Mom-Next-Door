f6a1343db08f5048f3b53f16090b3c18bd87b8b5
nuso <nelohjb@hotmail.com>
2019-06-01 21:41:39 +0800

    添加 Bubble 气泡.

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -1,9 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bubble : MonoBehaviour {
 
+    public float FadeTime = 0.35f;
+    public Text Text;
 
+    private Image background;
+    private float targetAlpha = 0f;
+    private float alphaVelocity = 0;
+
+    private float timer = 0f;
+
+
+    private void Start() {
+        background = GetComponent<Image>();
+        Fade(0f);   // 先隐身
+    }
+
+    private void Update() {
+        // 定时
+        if (timer > 0) {
+            timer -= Time.deltaTime;
+            targetAlpha = 1f;
+        }
+        else {
+            targetAlpha = 0f;
+        }
+
+        // 淡化动画
+        if (background.color.a != targetAlpha) {
+            float alpha = Mathf.SmoothDamp(background.color.a, targetAlpha, ref alphaVelocity, FadeTime);
+            Fade(alpha);
+        }
+    }
+
+    public void Bub(string text,float time = 3f) {
+        Text.text = text;
+        timer = time;
+    }
+
+    // 辅助--调整物体及子物体Alpha透明度
+    private void Fade(float alpha) {
+        Color BGColor = background.color;
+        Color TextColor = Text.color;
+        BGColor.a = alpha;
+        TextColor.a = alpha;
+        background.color = BGColor;
+        Text.color = TextColor;
+    }
 
 }
--- Assets/Scripts/test/test.cs
+++ Assets/Scripts/test/test.cs
@@ -6,9 +6,26 @@ public class test : MonoBehaviour
 {
 
     public QTE QTE;
+    public Bubble Bubble;
 
     void Start() {
         QTE.Play();
+        Bubble.Bub("嘿嘿嘿", 3f);
+    }
+
+    public void TestBubble() {
+        List<string> texts = new List<string>{
+            "哈哈哈哈哈",
+            "Nice boy",
+            "You Die",
+            "Audio",
+            "Bubble",
+            "Tesla 特斯拉",
+            "Attention!"
+        };
+
+        int index = Random.Range(0, texts.Count - 1);
+        Bubble.Bub(texts[index], Random.Range(2f, 4f));
     }
 
 }
