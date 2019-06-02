ba3e749d67388d1547ce5cf3e8c970935e2120a5
yyspyysp <45526192+yyspyysp@users.noreply.github.com>
2019-06-02 18:36:50 +0800

    Device进度条和qte启动

--- Assets/Scripts/Device.cs
+++ Assets/Scripts/Device.cs
@@ -1,9 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Device : MonoBehaviour {
+    public List<GameObject>QTE = new List<GameObject>();
+    public QTE qte;
+    public int Temp = 0;  //暂存心情
+    private int GameNum = 0;
+    private string QteName;
+    public GameObject GameList;
+    public GameObject ProgressBar;
+    private Progressbar progressbar;
+    public static Device instance;
+    public bool IsFinish = false;
+    private Transform Pos;
+    void Start()
+    {
+        instance = this;
+        progressbar = ProgressBar.GetComponent<Progressbar>();
+    }
+
+    void Update()
+    {
+        if (IsFinish)
+        {
+            CreateQte();
+            IsFinish = false;
+        }
+    }
+    public void ChooseGame()
+    {
+       
+    }
+    public void PlayQte(int n)
+    {
+       progressbar.C();
+       Pos=GameObject.Find("QtePos").transform;
+      
+        //根据玩家选择的游戏选择QTE
+          GameNum = n;
+        //生成进度条
+        Instantiate(ProgressBar.gameObject,Pos);
+       
+       
 
 
 
+
+    }
+
+    public void EndQte()
+    {
+        
+        //销毁QTE
+       Destroy(GameObject.Find(QteName));
+    }
+
+    public void CreateQte()
+    {
+       
+        GameObject NowQTE=Instantiate(QTE[GameNum],Pos);
+        NowQTE.transform.parent = Pos;
+
+
+    }
 }
---
+++ Assets/Scripts/Progressbar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progressbar : MonoBehaviour
+{
+   private Slider ProgressBar;
+    public float num = 0;
+   
+    public Text T;
+   
+   public Button B;
+    public bool IsFinish = false;
+    public bool CanFill = false;
+
+   
+    // Use this for initialization
+    void Start ()
+    {
+        ProgressBar = this.GetComponent<Slider>();
+     
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (CanFill)
+	    {
+	        DOTween.To(() => num, x => num = x, 101, 5);
+
+	        ProgressBar.value = (int) num;
+	        T.text = ProgressBar.value.ToString(CultureInfo.InvariantCulture) + "%";
+	       
+	        if (ProgressBar.value == 100)
+	        {
+	            CanFill = false;
+	            Device.instance.IsFinish = true;
+                Destroy(this.gameObject);
+	        }
+	    }
+	}
+
+    public void C()
+    {
+        CanFill = true;
+    }
+
+   
+    
+    
+}
