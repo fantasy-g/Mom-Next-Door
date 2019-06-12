24349685607e1c23b59408d0048c74e1031cd54a
muzixu <32829273+muzixu@users.noreply.github.com>
2019-06-12 11:56:43 +0800

    填充游戏主体

--- Assets/Scripts/Son.cs
+++ Assets/Scripts/Son.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+//[SerializeField]
 public enum SonState { homework, PC, PS4, nintendoSwitch, phone };
 public class Son : MonoBehaviour {
     public Device GameDevice=new Device();
@@ -8,16 +11,28 @@ public class Son : MonoBehaviour {
     public int Study;
     public int mood;//心情值关乎学习效率
     SonState SonStatus;
-
+    public GameObject Homework;
+    public List<GameObject> Games;
+    public GameObject StartInterface;
+    private GameObject go;
+    public GameObject GameList;
     private void Awake()
     {
         Joy = 0;
         Study = 0;
         mood = 50;
         SonStatus = SonState.homework;
+        Time.timeScale = 0;
+        go = new GameObject();
     }
     private void Update()
     {
+        if (Input.anyKeyDown && StartInterface.activeInHierarchy) {
+            StartInterface.SetActive(false);//进入游戏界面
+            Time.timeScale = 1;
+        }
+
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
             //触发收拾游戏机返回书桌的qte
@@ -28,19 +43,38 @@ public class Son : MonoBehaviour {
         //鼠标右键为聆听状态
         if (Input.GetMouseButtonDown(1))
         {
-            AudioManager.Instance.MomAudioPlay(AudioManager.MomClips.footstep);
+            // AudioManager.Instance.MomAudioPlay(AudioManager.MomClips.footstep);
+            Time.timeScale = 0.5f;
             //画面变成黑白
         }
         if (Input.GetMouseButtonUp(1))
         {
-            AudioManager.Instance.MomAudioPlay(AudioManager.MomClips.footstep);
+            // AudioManager.Instance.MomAudioPlay(AudioManager.MomClips.footstep);
+            Time.timeScale = 1f;
             //画面恢复彩色
         }
         switch (SonStatus)
         {
             case SonState.homework:
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    Homework.SetActive(false);
+                    Games[0].SetActive(false);
+                    Games[1].SetActive(true);
+                    //GameList.SetActive(true);                  
+                    SonStatus = SonState.PC;
+                    OpenComputer();
+                }
                 break;
-            case SonState.nintendoSwitch
+            case SonState.PC:
+                if (Input.GetKeyDown(KeyCode.Escape)){
+                    Homework.SetActive(true);
+                    SonStatus = SonState.homework;
+                    Games[0].SetActive(true);
+                    Games[1].SetActive(false);
+                    Games[2].SetActive(false);                     
+                }
+                break;
         }
 
     }
@@ -49,4 +83,23 @@ public class Son : MonoBehaviour {
         SonStatus = sonState;
 
     }
+    private void OpenComputer()
+    {
+        //播放开机音乐
+        go.transform.DOMoveZ(0.1f, 3f)
+            .OnComplete(new TweenCallback(OnGames));
+        
+    }
+    private void OnGames()
+    {
+        Games[1].SetActive(false);
+        Games[2].SetActive(true);
+    }
+    private void AddQte()
+    {
+        GameObject go = new GameObject();
+        go.transform.DOMoveZ(0.1f, 3f)
+            .OnComplete(new TweenCallback(OnGames));
+    }
+
 }
