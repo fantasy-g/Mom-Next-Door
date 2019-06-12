bf52136322d89d2800695177abf9d95de0b3375d
wangtianci <377090139@qq.com>
2019-06-12 13:52:59 +0800

    完善游戏结构 结局检测 待添加结局画面

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+public enum Endings { normalend, goodend, badend, trueend };//普通结局，好学生结局，差生结局，学神结局
 public class LevelManager : MonoBehaviour
 {
     private static LevelManager _instance;
     public static LevelManager Instance { get { return _instance; } }
 
+    
     public Mom mom;
     public Son son;
 
+    public Text ShowTimeText;
+    public float SchoolTime = 28800f;
+    public int timespeed = 1;
+    public int hour;
+    public int minute;
+    public int second;
+
+    [SerializeField]
     private bool son_is_happy = false;//心情值是否到100
+    [SerializeField]
     private bool son_is_workinghard = false;//学习值是否到100
+    [SerializeField]
     private bool mom_discover_son = false;//是否被妈妈发现玩游戏
+
+    private bool gameover=false;
 
     private void Awake()
     {
@@ -28,12 +43,36 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    public void CheckGameOver()
+    private void Update()
     {
+        if (SchoolTime <= 0 && gameover == false)
+        {
+            SchoolTime = 0;
+            CheckEndings();
+            gameover = true;
+        }
+        else
+            SchoolTime -= Time.deltaTime * timespeed;
+
+        hour = (int)SchoolTime / 3600;
+        minute = (int)(SchoolTime - hour * 3600) / 60;
+        second = (int)(SchoolTime - hour * 3600 - minute * 60);
+
+        ShowTimeText.text = string.Format("{0:D2}:{1:D2}:{2:D2}",
+                                            hour, minute, second);
+    }
+
+    public void GameOver()
+    {
+        //游戏结束 先用TimeScale表现
+        // Time.timeScale = 0;
         //被麻麻发现
+        if (gameover == true)
+            return;
         if(mom_discover_son)
         {
             //被发现玩游戏 被揍 普通结局 
+            Debug.Log("NormalEnding");
             return;
         }
         //正常结尾
@@ -42,30 +81,80 @@ public class LevelManager : MonoBehaviour
             if(!son_is_happy)
             {
                 //没爽到 但努力学习 好学生结局
+                Debug.Log("GoodEnding");
                 return;
             }
             else if(!son_is_workinghard)
             {
                 //爽到 但没努力学习 差生结局
+                Debug.Log("BadEnding");
                 return;
             }
             else
             {
                 //既爽到 又努力学习 学神结局
+                Debug.Log("TrueEnding");
                 return;
             }
         }
         else
         {
             //既没有爽到也没有学习
+            Debug.Log("CaiEnding");
             return;
         }
     }
+    
+    //public void CheckEndings(Endings ending)
+    //{
+    //    switch(ending)
+    //    {
+    //        case Endings.normalend:
+    //            mom_discover_son = true;
+    //            break;
+    //        case Endings.goodend:
+    //            son_is_workinghard = true;
+    //            break;
+    //        case Endings.badend:
+    //            break;
+    //        case Endings.trueend:
+    //            son_is_workinghard = true;
+    //            son_is_happy = true;
+    //            break;
+    //        default:
+    //            break;
+    //    }
+    //    CheckGameOver();
 
-    public void CheckParameter()
+    //}
+    public void CheckEndings()
     {
-        //游戏结束时比gameover先调用，用于检查参数
+        if(SchoolTime!=0)
+        {
+            mom_discover_son = true;
+        }
+        else
+        {
+            mom_discover_son = false;
+
+            if (son.Joy == 100)
+            {
+                son_is_happy = true;
+            }
+            else
+            {
+                son_is_happy = false;
+            }
+            if (son.Study == 100)
+            {
+                son_is_workinghard = true;
+            }
+            else
+            {
+                son_is_workinghard = false;
+            }
+        }
+
+        GameOver();
     }
-
-
 }
